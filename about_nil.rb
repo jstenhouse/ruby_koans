b2315c9b652c12422a6dbae221e3dedf9b037f1d
Jason Stenhouse <jason.stenhouse@gmail.com>
2013-12-14 09:39:26 -0800

    iterations

--- about_nil.rb
+++ about_nil.rb
@@ -13,7 +13,6 @@ class AboutNil < Neo::Koan
       nil.some_method_nil_doesnt_know_about
     rescue Exception => ex
       # What exception has been caught?
-      puts ex
       assert_equal NoMethodError, ex.class
 
       # What message was attached to the exception?
